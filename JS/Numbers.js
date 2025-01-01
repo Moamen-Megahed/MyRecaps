ec38adb2abcaef225cbd6230761450543685e122
Moamen Megahed <moamenmegahed22@gmail.com>
2025-01-01 15:12:18 +0200

    add js numbers

---
+++ JS/Numbers.js
@@ -0,0 +1,60 @@
+/*
+Number Methods
+
+-input number 
+
+*1- num.toString() 
+
+2- num.toFixed(2) => Takreb le 2 ba3d el3alama 
+
+----------------------------------------------------
+
+-output number 
+
+*3- parseInt(str) // parseFloat(str) 
+
+----------------------------------------------------
+
+- Number object
+
+1- Number.isInteger(any) => true or false 
+
+2- Number.isNan(any) => true or false
+
+3- isFinite(any) => true or false 
+
+------------------------------------------------------
+
+- Math object
+
+1- Math.round(num) => bekrb
+
+2- Math.ceil(num) => bekrb lfo2
+
+3- Math.floor(num) => bekrb lt7t
+
+4- Math.trunc(num) => bekrb lt7t
+
+5- Math.min(num,num,...)
+
+6- Math.max(num,num,...) 
+
+7- Math.random()
+
+8- Math.pow(2,4) => 2 os 4
+
+9- Math.sqrt()
+
+// */  
+
+// let num = 6.5556165;
+// let str = " moamen aaa"
+// let nan = +"Moamen"
+
+
+// console.log();
+
+
+
+
+
