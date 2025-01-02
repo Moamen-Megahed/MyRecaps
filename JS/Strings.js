8935895f6e2c517c9c6bbaff578ad3a518c7f78d
Moamen Megahed <moamenmegahed22@gmail.com>
2025-01-02 15:46:48 +0200

    JS - strings

---
+++ JS/Strings.js
@@ -0,0 +1,53 @@
+/*
+String Methods
+
+- input string 
+
+*1- parseInt(str),parseFloat(str) *output number*
+
+2- str.charAt(index) = str[index]
+
+3- str.lenght
+
+4- str.trim() =>remove spaces at the beginning and the end
+
+5- str.toUpperCase() // str.toLowerCase()
+
+*6-str.indexOf("el7arf", start) => output index(number) OR -1 yaany mafesh
+
+*6-str.LastIndexOf("el7arf", start) => bymshy mn ala5r
+
+7- str.slice(start index, end index)
+
+8- str.repeat(number)
+
+*9- str.split( "Mnen" ,kam marra) *output array*
+
+10- str.substring(start index, end index) => like slice but not accept negative
+
+11- str.substr(start index, num of charachters)
+
+*12- str.includes("word or letter, start index") => true or false
+
+*12- str.startsWith("word or letter, start index") => true or false
+
+*12- str.endsWith("word or letter, start index") => true or false
+
+13- str.replace("",""), str.replaceAll("","")
+
+
+
+------------------------------------------------------------
+- output string
+
+13- num.toString() *input number*
+
+14- arr.join(seperator) => *input array*
+*/
+
+
+let str = "    moamen      ";
+
+console.log(str.slice())
+
+
