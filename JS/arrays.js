e37282575d503b651ae205fc2d2c659bda60f6e6
Moamen Megahed <moamenmegahed22@gmail.com>
2025-01-12 16:59:43 +0200

    JS - Arrays

---
+++ JS/arrays.js
@@ -0,0 +1,66 @@
+/*
+Array methods
+
+-input array
+
+1- arr.length => add update control  
+
+2-arr.unshift("","",..) => add from first
+
+3-arr.push("","",..) => add from end
+
+*4-arr.shift() => remove the first index and bt5znoh  *output first index*
+
+*5-arr.pop() => add the end index and bt5znoh *output last index*
+
+*6- arr.indexOf(element,start) / arr.lastIndexOf(element,start) => like string
+
+*7- arr.includes(element) => true or false
+
+8- arr.sort() -> change the array
+
+9- arr.reverse() -> change the array
+
+10- arr.slice(start, end) => all like string
+
+11- arr.splice(start, deleted count, "addArr1","addArr2") => add and remove ,=> change the array
+
+12- arr1.concat(arr2,arr3,"ele",...) => in new array
+
+*13- arr.join(seperator) => *output string*
+
+
+14- arr.map(function(e,index,array){})
+
+15- arr.filter(function(e,index,array){})
+
+*16-arr.reduce(function(acc,cur,index,array){}) *بتعمل علي العناصر عمليه وبترجع عنصر واحد*
+
+*17-arr.forEach(function(e,index,array){}) *مش بترجع حاجه*
+
+*18-arr.some(function(e,index,array){}) => one true *true or false*
+
+*19-arr.every(function(e,index,array){}) => All true *true or false*
+
+
+19- arr.copyWithin(Target,start[opt],end[opt]) => copy from the array to another place in the same array
+    target => the new place , start,end => copy from  to
+-----------------------------------------------------------------------------------------------
+
+-output array
+
+*- str.split("mnen", kammarra) *input string*
+
+*- Object.keys(obj-name) => array of the props name *input obj*
+
+*- str.match(regEx) *array of elements that have the pattern*
+-----------------------------------------------------------------------------------------------
+
+Array object
+
+1- Array.isArray(arr) => true or false
+
+2- Array.from(iterable,Map func) => the iterable can be string,set,arguments"in functions"[the word litterly] to get an array from them
+
+*/ 
+
